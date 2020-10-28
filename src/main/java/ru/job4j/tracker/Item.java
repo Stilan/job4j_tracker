580c44824cc8a624bb00f53b17ebda9153138a09
Stilan <40999153+Stilan@users.noreply.github.com>
2020-10-28 17:47:30 +0700

    2. Перегрузка конструктора [#374773]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -7,17 +7,7 @@ public class Item {
     private String name;
     private LocalDateTime created = LocalDateTime.now();
 
-    public Item() {
-    }
 
-    public Item(int id) {
-        this.id = id;
-    }
-
-    public Item(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
 
     public LocalDateTime getCreated() {
         return created;
