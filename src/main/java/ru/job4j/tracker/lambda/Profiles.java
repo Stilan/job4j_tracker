81c00bd81fc4504a423363dd18c63e575b5a3425
Stilan <40999153+Stilan@users.noreply.github.com>
2021-02-08 15:08:29 +0700

    3. Уникальность элементов и сортировка. [#374524]

--- src/main/java/ru/job4j/tracker/lambda/Profiles.java
+++ src/main/java/ru/job4j/tracker/lambda/Profiles.java
@@ -1,11 +1,14 @@
 package ru.job4j.tracker.lambda;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
     public List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().
-                map(Profile::getAddress).collect(Collectors.toList());
+        return profiles.stream()
+                .map(Profile::getAddress).sorted(new AddressComparator()).distinct().
+                collect(Collectors.toList());
     }
+
 }
--- src/test/java/ru/job4j/tracker/lambda/ProfilesTest.java
+++ src/test/java/ru/job4j/tracker/lambda/ProfilesTest.java
@@ -15,22 +15,24 @@ public class ProfilesTest  {
     @Before
     public void setUp() {
         profileList.add(new Profile(new Address("Москва", "Ленина", 12, 43)));
-        profileList.add(new Profile(new Address("Н-Новгород", "Гогаля", 14, 4)));
-        profileList.add(new Profile(new Address("Мурманск", "Гогаля", 12, 5)));
+        profileList.add(new Profile(new Address("Н-Новгород", "Гоголя", 14, 4)));
+        profileList.add(new Profile(new Address("Мурманск", "Гоголя", 12, 5)));
         profileList.add(new Profile(new Address("Екатеренбург", "Ленина", 16, 3)));
-        profileList.add(new Profile(new Address("Челябенск", "Гогаля", 43, 8)));
+        profileList.add(new Profile(new Address("Москва", "Гоголя", 43, 8)));
+        profileList.add(new Profile(new Address("Москва", "Гоголя", 43, 8)));
+        profileList.add(new Profile(new Address("Москва", "Гоголя", 43, 8)));
     }
 
     @Test
-    public void whenCollectClassA() {
+    public void ProfilesTest1() {
         Profiles profiles = new Profiles();
         List<Address> addressList = profiles.collect(profileList);
         List<Address> expected = new ArrayList<>();
-        expected.add(new Address("Москва", "Ленина", 12, 43));
-        expected.add(new Address("Н-Новгород", "Гогаля", 14, 4));
-        expected.add(new Address("Мурманск", "Гогаля", 12, 5));
         expected.add(new Address("Екатеренбург", "Ленина", 16, 3));
-        expected.add(new Address("Челябенск", "Гогаля", 43, 8));
+        expected.add(new Address("Москва", "Ленина", 12, 43));
+        expected.add(new Address("Москва", "Гоголя", 43, 8));
+        expected.add(new Address("Мурманск", "Гоголя", 12, 5));
+        expected.add(new Address("Н-Новгород", "Гоголя", 14, 4));
         assertThat(addressList, is(expected));
 
     }
