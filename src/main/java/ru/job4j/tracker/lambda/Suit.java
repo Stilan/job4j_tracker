27c008bec6c1d1367ced75983b13014e77e86be3
Stilan <40999153+Stilan@users.noreply.github.com>
2021-02-10 17:58:38 +0700

    8. Перенос методов в Stream API [#374529]

---
+++ src/main/java/ru/job4j/tracker/lambda/Suit.java
@@ -0,0 +1,5 @@
+package ru.job4j.tracker.lambda;
+
+public enum Suit {
+    Diamonds, Hearts, Spades, Clubs
+}
---
+++ src/main/java/ru/job4j/tracker/lambda/Value.java
@@ -0,0 +1,5 @@
+package ru.job4j.tracker.lambda;
+
+public enum Value {
+    V_6, V_7, V_8
+}
